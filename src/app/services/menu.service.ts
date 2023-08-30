b633c8ba53b684719a142ca95f55da08ce47c306
Gastón Bonanno <gbonanno-dev@gmail.com>
2023-08-30 20:12:44 -0300

    feat(Toke): WIP

--- src/app/services/menu.service.ts
+++ src/app/services/menu.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import {CreateUserRequest, CreateUserResponse, LoginUserRequest, LoginUserResponse} from "../interfaces/user.interface";
-import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {HttpClient, HttpHeaders, HttpParams} from "@angular/common/http";
 import {catchError, map, Observable, tap} from "rxjs";
 import {MenuRequest, MenuResponse} from "../interfaces/menu.interface";
+import {SecureService} from "../utils/secure.service";
 
 const URL = environment.url;
 
@@ -12,11 +13,15 @@ const URL = environment.url;
 })
 export class MenuService {
 
-  constructor( private http: HttpClient) {
+  constructor( private http: HttpClient, private secureService: SecureService) {
   }
 
-  getMenu(): Observable<MenuResponse> {
-    return this.http.get<MenuResponse>( `${URL}/company-menu/1`)
+  getMenuList(): Observable<MenuResponse[]> {
+    // let params = new HttpParams()
+    //   .set('companyId', companyId);
+    // return this.http.get<MenuResponse>( `${URL}/company-menu/`, {params: params})
+    // return this.http.get<MenuResponse>( `${URL}/company-menu/`)
+    return this.secureService.get('company-menu');
   }
 
   saveMenu(menu: MenuRequest): Observable<MenuResponse> {
--- src/app/utils/secure.service.ts
+++ src/app/utils/secure.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import {CreateUserRequest, CreateUserResponse, LoginUserRequest, LoginUserResponse} from "../interfaces/user.interface";
 import {HttpClient, HttpHeaders} from "@angular/common/http";
-import {catchError, from, map, Observable, tap} from "rxjs";
+import {catchError, from, map, mergeMap, Observable, tap} from "rxjs";
 import {TokenService} from "../services/token.service";
 
 const URL = environment.url;
@@ -18,46 +18,50 @@ export class SecureService {
   postLogin(path: string, body: any | null): Observable<any> {
     return this.http.post<any>(`${URL}/${path}`, body)
   }
-
-  async post(path: string, body: any | null): Promise<Observable<any>> {
-    return await this.createTokenHeader().then(httpHeaders => {
-      return this.http.post<any>(`${URL}/${path}`, body, {headers: httpHeaders})
-    })
-  }
+  //
+  // async post(path: string, body: any | null): Promise<Observable<any>> {
+  //   return await this.createTokenHeader().then(httpHeaders => {
+  //     return this.http.post<any>(`${URL}/${path}`, body, {headers: httpHeaders})
+  //   })
+  // }
 
   get(path: string): Observable<any> {
-    const responsePromise = this.createTokenHeader().then((httpHeaders) => {
-      this.http.get<any>(`${URL}/${path}`, {headers: httpHeaders}).subscribe((resp) => {
-        return resp
-      })
-    })
-    return from(responsePromise)
+    // const responsePromise = this.createTokenHeader().then((httpHeaders) => {
+    // return this.createTokenHeader().pipe(
+    //   mergeMap((header) => this.http.get<any>(`${URL}/${path}`, {headers: header})),
+    // )
+    let algo: string = "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjp7ImNvbXBhbnlJZCI6MSwiZW1haWwiOiJwcnVlYmFFbWFpbCIsIm5hbWUiOiJjb21wYW5pYSBsb2NhIiwiY3VpdCI6IjEyMzQ1NDUzNCIsImFkZHJlc3MiOiJDYWxsZSBmYWxzYSAxMjMiLCJwaG9uZU51bWJlciI6IjExMzQ0NDU2NjQifSwiZXhwIjoxNjkzNDM2NTc5fQ.qsw3mWM9bU3az9GuXe-GGuwEtW6NxcgZTf8U-Qw50AY"
+    let header = new HttpHeaders({
+      'auth-token': algo
+    });
+    return this.http.get<any>(`${URL}/${path}`, {headers: header})
   }
+  //
+  // patch(path: string, body: any | null): Observable<any> {
+  //   const responsePromise = this.createTokenHeader().then((httpHeaders) => {
+  //     this.http.patch<any>(`${URL}/${path}`, body, {headers: httpHeaders}).subscribe((resp) => {
+  //       return resp
+  //     })
+  //   })
+  //   return from(responsePromise)
+  // }
+  //
+  // delete(path: string): Observable<any> {
+  //   const responsePromise = this.createTokenHeader().then((httpHeaders) => {
+  //     this.http.delete<any>(`${URL}/${path}`, {headers: httpHeaders}).subscribe((resp) => {
+  //       return resp
+  //     })
+  //   })
+  //   return from(responsePromise)
+  // }
 
-  patch(path: string, body: any | null): Observable<any> {
-    const responsePromise = this.createTokenHeader().then((httpHeaders) => {
-      this.http.patch<any>(`${URL}/${path}`, body, {headers: httpHeaders}).subscribe((resp) => {
-        return resp
-      })
-    })
-    return from(responsePromise)
-  }
-
-  delete(path: string): Observable<any> {
-    const responsePromise = this.createTokenHeader().then((httpHeaders) => {
-      this.http.delete<any>(`${URL}/${path}`, {headers: httpHeaders}).subscribe((resp) => {
-        return resp
-      })
-    })
-    return from(responsePromise)
-  }
-
-  private createTokenHeader(): Promise<HttpHeaders> {
-    return this.tokenService.getToken().then((storedToken) => {
+  private createTokenHeader(): Observable<HttpHeaders> {
+    const promiseHttpHeader =  this.tokenService.getToken().then((storedToken) => {
       return new HttpHeaders({
         'auth-token': storedToken ? storedToken : ''
       });
     })
+    return from(promiseHttpHeader)
   }
 
 }
