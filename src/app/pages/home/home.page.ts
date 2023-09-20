a1c724107adf480a104a8b322bf17c5bf2a24206
Paula <paulalezcano21@gmail.com>
2023-09-20 18:31:25 -0300

    chore: css item y home

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -14,6 +14,7 @@ import {MenuResponse} from "../../interfaces/menu.interface";
   imports: [IonicModule, CommonModule, FormsModule]
 })
 export class HomePage implements OnInit {
+  datetime: any;
 
 
   constructor(private menuService: MenuService, private navCtrl: NavController, private toast: Toast) {
