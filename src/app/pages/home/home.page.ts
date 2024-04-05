7434dbcea683457ad7bf415e5b8aa103d0f4ac0a
mfraga710 <82245582+mfraga710@users.noreply.github.com>
2024-04-05 09:39:29 -0300

    Mejora estetica del home page. Las cards ahora redirigen

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -4,7 +4,9 @@ import { FormsModule } from '@angular/forms';
 import {IonicModule, NavController} from '@ionic/angular';
 import {MenuService} from "../../services/menu.service";
 import {Toast} from "../../utils/toast";
+import {UserService} from "../../services/user.service";
 import {MenuResponse} from "../../interfaces/menu.interface";
+import {ProfileData} from "../../interfaces/user.interface";
 
 @Component({
   selector: 'app-home',
@@ -15,12 +17,29 @@ import {MenuResponse} from "../../interfaces/menu.interface";
 })
 export class HomePage implements OnInit {
   datetime: any;
+  profileData: ProfileData = {
+    name: undefined,
+    cuit: undefined,
+    address: undefined,
+    phoneNumber: undefined,
+  };
 
 
-  constructor(private menuService: MenuService, private navCtrl: NavController, private toast: Toast) {
+  constructor(private userService: UserService, menuService: MenuService, private navCtrl: NavController, private toast: Toast) {
   }
 
+  navigateTo(path: string){
+    this.navCtrl.navigateRoot(path, {animated: true}).then()
+  }
   ngOnInit() {
+    this.userService.getCompanyData().subscribe({
+      next: (resp: ProfileData) => {
+        this.profileData.name = resp.name
+    },
+      error: (err) =>{
+        console.log('error',err);
+        this.toast.present('bottom', 'Error al obtener datos del perfil').then();
+      },
+    });
   }
-
 }
